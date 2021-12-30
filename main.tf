6f1b8d825ac31dc30e1bcad9348edc65f871af76
suhaskarnik <suhaskarnik@gmail.com>
2021-12-30 10:55:21 +0530

    first commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+    region = var.region
+    profile = var.profile
+}
+
+locals {
+  addl_tags = {
+      project = "Learn LB"
+  }
+  azs = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+  address_space = "10.0.0.0/16"
+  subnets = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24","10.0.4.0/24" ]
+  azs = local.azs
+  addl_tags = local.addl_tags
+}
+
+# module "vm" {
+#   source = "./modules/vm"
+#   vpc_id = module.vpc.id
+#   subnet_ids = module.vpc.subnet_ids
+
+#   addl_tags = local.addl_tags
+# }
+
+module "iam" {
+  source = "./modules/iam"
+  addl_tags = local.addl_tags
+}
+
+module "asg" {
+  source = "./modules/asg"
+  iam_instance_profile_name = module.iam.iam_instance_profile.name
+  vpc = module.vpc.id
+  # azs = local.azs
+  subnet_ids = module.vpc.subnet_ids
+  addl_tags = local.addl_tags
+}
+
+
+
+output "subnets" {
+  value = module.vpc.subnet_ids
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  description = "The region Terraform deploys your instance"
+  default = "us-east-1"
+}
+
+variable "profile"{
+  type = string
+  default = "acg"
+}
+
